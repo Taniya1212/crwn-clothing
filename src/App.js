f381022a91ab4f60fbe19f36cb9e0d382f0b97c3
Taniya1212 <taniyachaturvedi08@gmail.com>
2020-04-23 07:11:55 +0530

    checkout-item component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import {auth,createUserProfileDocument } from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions'
+import {selectCurrentUser} from './redux/user/user.selector';
+import {createStructuredSelector} from 'reselect';
 
 class  App extends React.Component {
   unSubscribeFromAuth = null
@@ -46,15 +49,15 @@ class  App extends React.Component {
       <Route exact path='/' component={HomePage} ></Route>
       <Route exact path='/shop' component={ShopPage} ></Route>
       <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to = '/' />) : (<SignInAndSignUp></SignInAndSignUp>)}  ></Route>
-
+      <Route exact path='/checkout' component={CheckoutPage} ></Route>
       </Switch>
       
     </div>
   );
 }}
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps   = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,10 @@ import {ReactComponent as Logo} from '../../assets/original.svg'
 import {auth} from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import {createStructuredSelector} from 'reselect';
+import {selectCartHidden} from '../../redux/cart/cart.selectors';
+import {selectCurrentUser} from '../../redux/user/user.selector';
+
 
 const Header = ({currentUser,hidden}) => (
 <div className = 'header'>
@@ -36,9 +40,9 @@ const Header = ({currentUser,hidden}) => (
 </div>
 );
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector ({
+    currentUser : selectCurrentUser,
+    hidden: selectCartHidden
 
 })
 
